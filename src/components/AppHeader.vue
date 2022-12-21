f42b699ee8afd62be2277fb9e85c93c832df0a21
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-21 13:07:38 +0100

    header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -35,7 +35,7 @@ export default{
 
 <template>
     <section class="my_header d-flex align-items-center">
-        <div class="d-flex align-items-center justify-content-between w-100">
+        <div class="container d-flex align-items-center justify-content-between w-100">
 
             <!-- Logo -->
             <div class="my_logo">
@@ -44,14 +44,30 @@ export default{
                 </a>
             </div>
 
-            <!-- Nav Bar -->
-            <nav class="d-flex align-items-center">
-                <ul class="d-flex gap-5 align-items-center">
-                    <li v-for="link in links" class="text-white">
-                        {{link.linkName}}
-                    </li>
-                </ul>
-            </nav>
+            <!-- Nav Bar Container -->
+            <div class="d-flex align-items-center gap-5">
+                <!-- Nav Bar -->
+                <nav class="d-flex align-items-center">
+                    <ul class="d-flex gap-5 align-items-center">
+                        <li v-for="link in links" class="text-white d-flex align-items-center gap-2">
+                            <span class="fw-bold ">{{link.linkName}}</span>
+                            <!-- New Link Condition -->
+                            <div class="my_new_link d-flex justify-content-center align-items-center text-dark" v-if="link.linkName === 'Store'">
+                                <span>new</span>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+    
+                <!-- Button -->
+                <button class="my_button rounded-pill">Schedule a workout</button>
+    
+                <!-- Icon Input -->
+                <div class="text-white d-flex gap-4">
+                    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                    <font-awesome-icon icon="fa-solid fa-cart-shopping" />
+                </div>
+            </div>
 
         </div>
     </section>
@@ -67,5 +83,16 @@ export default{
 }
 .my_logo{
     width: 200px;
+}
+.my_new_link{
+    padding: 5px;
+    border-radius: 5px;
+    background-color: #fff941;
+}
+.my_button{
+    background-color: #4154ff;
+}
+.my_button:hover{
+    background-color: #ff414b;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -8,10 +8,10 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass,  faCartShopping} from '@fortawesome/free-solid-svg-icons'
 
 /* add icons to the library */
-library.add(faMagnifyingGlass)
+library.add(faMagnifyingGlass, faCartShopping)
 
 
 createApp(App)
