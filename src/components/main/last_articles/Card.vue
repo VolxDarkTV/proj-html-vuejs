f063b9bd1985e1416b45e03cfd25263cec17e2b0
VolxDarkTV <vinvenzo.foti@outlook.it>
2022-12-21 20:43:35 +0100

    fixed hover effect on card

--- src/components/main/last_articles/Card.vue
+++ src/components/main/last_articles/Card.vue
@@ -20,7 +20,7 @@
 
                 <!-- Img -->
                 <div class="my_image">
-                    <img :src="item.img" alt="">
+                    <img class="my_image_mask" :src="item.img" alt="">
                 </div>
 
                 <!-- info -->
@@ -42,6 +42,17 @@
 // Card css
 .my_image{
     width: 400px;
+    position: relative;
+}
+.my_image:hover:after{
+    content:'';
+    position:absolute;
+    left:0; 
+    top:0;
+    width:100%; 
+    height:100%;
+    background: linear-gradient(to bottom, rgba(0,47,75,0) 0%,rgba(99, 24, 228, 0.7) 100%)
+    // mask-image: linear-gradient(to bottom, rgb(145, 0, 203), rgba(161, 0, 113, 0.7));
 }
 .my_paragraph{
     color: #9d929b;
